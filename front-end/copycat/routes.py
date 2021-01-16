1a76543ee8fb0a1bed7d6ff35d8acb34802920c8
j-clements <70959739+j-clements@users.noreply.github.com>
2021-01-16 18:13:24 -0500

    add script ability on gamesession

--- front-end/copycat/routes.py
+++ front-end/copycat/routes.py
@@ -18,8 +18,7 @@ def home():
 def settings():
     random_number = random.randint(1, 10000000)
     name = request.args.get('name_input')
-    time_options = ["30 sec", '40 sec', '50 sec', '60 sec']
-    return render_template('settings.html', name=name, random_number=random_number, times=time_options)
+    return render_template('settings.html', name=name, random_number=random_number)
 
 @app.route("/game", methods=['GET', 'POST'])
 def game():
@@ -27,5 +26,7 @@ def game():
     session_id = session.session_id
     token = opentok.generate_token(session_id)
     sessionNr = request.args.get('session')
-    time = request.args.get('round_time')
-    return render_template('gamesession.html',session_number=sessionNr, round_time=time, api_key=key, session_id=session_id, token=token)
+    if request.method == "POST":
+
+        print("it worked")
+    return render_template('gamesession.html',session_number=sessionNr, api_key=key, session_id=session_id, token=token)
